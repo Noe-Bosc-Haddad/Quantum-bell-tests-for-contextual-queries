4d6ee89c2e3f6f90304b50b31f731bd7ea985d01
Chaostellaire <harmonie.lebrun1@gmail.com>
2024-09-12 15:53:14 +0200

    can now write json for HAL storage, but documents not splits ?

--- HAL.py
+++ HAL.py
@@ -1,17 +1,14 @@
+from typing import Tuple
 import numpy as np
-from formattext import text2vec
 import json
 
 
-global WASTEWORDS
-WASTEWORDS = "the a it they their its his her him them"
 
-def Hal_construct(input_array:np.ndarray, window_size:int = 10) -> np.ndarray:
-    global WASTEWORDS 
+def Hal_construct(input_array:np.ndarray, window_size:int = 10) -> Tuple[np.ndarray, dict, dict]:
     ## get word vec
     unique_words = np.unique(input_array)
+    WASTEWORDS = ["the","a","their","theirs","it","its", "they","an"]
     ## destroy waste words
-    WASTEWORDS = text2vec(WASTEWORDS)
     for waste in WASTEWORDS :
         unique_words = np.delete(unique_words, np.where(unique_words == waste))
 
@@ -27,14 +24,13 @@ def Hal_construct(input_array:np.ndarray, window_size:int = 10) -> np.ndarray:
         word2int[unique_words[i]] = i
         int2word[i] = unique_words[i]
     #don't need unique_words anymore
-    print(word2int)
     unique_words = False
     # completing HAL
-    for i in range(input_array.size) :
+    for i in range(len(input_array)) :
 
         if not input_array[i] in WASTEWORDS :
             j = i + 1
-            while j < input_array.size and j-i<=window_size:
+            while j < len(input_array) and j-i<=window_size:
 
                 if not input_array[j] in WASTEWORDS:
 
@@ -42,7 +38,7 @@ def Hal_construct(input_array:np.ndarray, window_size:int = 10) -> np.ndarray:
                 j +=1
                 
 
-    return hal
+    return hal, word2int, int2word
 
 
 def json_extractor(path:str):
@@ -52,11 +48,13 @@ def json_extractor(path:str):
 
 def makecorpushals(path:str, window_size : int =10 ) -> None:
     documentiterator = json_extractor(path)
-    hal_list = []
+    dump = {}
+    i = 0
     for document in documentiterator:
-        hal_list.append(Hal_construct(np.array(document), window_size))
+        (HAL, word2int, int2word) = Hal_construct(document, window_size)
+        dump["document{i}"] = {"HAL" : HAL.tolist(), "word2int":word2int, "int2word":int2word}
     with open("./hal_matrices.json", 'w') as halw :
-        json.dump(hal_list, halw, intend = 4)
+        json.dump(dump, halw)
 
 
     
@@ -65,5 +63,3 @@ def makecorpushals(path:str, window_size : int =10 ) -> None:
 if __name__ == "__main__" :
     input = np.array(["the", "dog", 'runs', 'through', 'the', 'backyard', 'the', 'backyard', 'is','no','more', 'clean'])
     makecorpushals('./wikipedia_documents_cleaned.json')
-    with open("./hal_matrices.json") as f :
-        print(json.load(f)[0])
